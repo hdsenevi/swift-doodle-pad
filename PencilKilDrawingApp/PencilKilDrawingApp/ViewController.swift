7a7ba0d13c304727f20a19612168f2d17be2ff01
hdsenevi <shanaka36@gmail.com>
2021-02-15 17:15:12 +1100

    Initial project setup

---
+++ PencilKilDrawingApp/PencilKilDrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PencilKilDrawingApp
+//
+//  Created by Shanaka Senevirathne on 15/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
